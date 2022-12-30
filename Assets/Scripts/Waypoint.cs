7bcf31970023779c291e7216d8268c69a45f43d5
Ocomic <108867917+Ocomic@users.noreply.github.com>
2022-12-30 01:33:17 +0100

    Targeting enemies
    - added weapon
    - weapon can target enemies
    - script for aiming at enemies

--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Waypoint : MonoBehaviour
 {
     [SerializeField] bool isPlaceble;
+    [SerializeField] GameObject towerPrefab;
     // Start is called before the first frame update
     private void OnMouseDown()
     {
         if(isPlaceble)
         {
-            Debug.Log(transform.name);
+            Instantiate(towerPrefab, transform.position, Quaternion.identity);
+            isPlaceble = false;
         }
         
     }
